1242c913b2c707542d01f691c8bc0f9d8957b728
kabu1 <kkabubenson@gmail.com>
2020-09-08 15:10:51 -0400

    add error handler

--- app/__init__.py
+++ app/__init__.py
@@ -13,3 +13,4 @@ app.config.from_pyfile('config.py')
 bootsrap = Bootstrap(app)
 
 from app import views
+from app import error
--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from flask import render_template
 from app import app
-from .request import get_movies
+from .request import get_movies,get_movie
+
+
+
 # Views
 @app.route('/')
 def index():
@@ -13,16 +16,17 @@ def index():
     popular_movies = get_movies('popular')
     upcoming_movie = get_movies('upcoming')
     now_showing_movie = get_movies('now_playing')
-
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template ('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
-@app.route('/movie/<movie_id>')
-def movie(movie_id):
+
+@app.route('/movie/<int:id>')
+def movie(id):
 
     '''
     View movie page function that returns the movie details page and its data
     '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
 
-    title = 'You are viewing {movie_id}'
-    return render_template('movie.html', title = title)
+    return render_template('movie.html',title = title,movie = movie)
